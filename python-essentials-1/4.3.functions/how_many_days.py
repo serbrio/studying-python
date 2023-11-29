7f05d7c6d9d9cc62081673b0638bf79d61772ae5
serbrio <28647145+serbrio@users.noreply.github.com>
2023-11-29 03:57:26 +0100

    python-essentials-1: 4.3: how_many_years.py: Add check year < 1582

--- python-essentials-1/4.3.functions/how_many_days.py
+++ python-essentials-1/4.3.functions/how_many_days.py
@@ -4,7 +4,7 @@ from leap_years import is_year_leap
 
 
 def days_in_month(year, month):
-    if month not in range(1, 13):
+    if month not in range(1, 13) or year < 1582:
         return None
     days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month == 2 and is_year_leap(year):
@@ -13,9 +13,9 @@ def days_in_month(year, month):
         return days[month - 1]
 
 
-test_years = [1900, 2000, 2016, 1987, 1999]
-test_months = [2, 2, 1, 11, 15]
-test_results = [28, 29, 31, 30, None]
+test_years = [1900, 2000, 2016, 1987, 1999, 1581]
+test_months = [2, 2, 1, 11, 15, 3]
+test_results = [28, 29, 31, 30, None, None]
 for i in range(len(test_years)):
     yr = test_years[i]
     mo = test_months[i]

e535c2afe691bf3428a29299f197e4772249828d
serbrio <28647145+serbrio@users.noreply.github.com>
2023-12-20 15:31:24 +0100

    en_spelling: refactoring 1. Add function generate_links_for_words(words, n), wich generates dict with n words 2. Make play_sound ignore keyboard while playing 3. Add Welcome note 4. Other refactoring

--- en_spelling/spell_checker.py
+++ en_spelling/spell_checker.py
@@ -18,16 +18,26 @@ def get_words(file):
 
 
 def get_audio_link(word):
-    response = scrape("en", "en", word)
+    """Get and return link to audio file on wiktionary.org
+    for the given word in English.
+    The first found link of any type (wav, ogg, mpeg)
+    is used.
+
+    """
+    response = scrape("en", "en", word) 
+    # scrape does return translations, so
+    # workaround used: translation from en to en
     for d in response['pronunciation']:
         for val in d['values']:
             if val['type'] in ['audio/wav', 'audio/ogg', 'audio/mpeg']:
-                #print(val['value'])
                 return "https:" +  val['value']
 
 
 def play_sound(link):
-    subprocess.call(["mplayer", "-nolirc", "-really-quiet", link, "&> /dev/null"])
+    subprocess.call([
+        "mplayer", "-nolirc", "-really-quiet",
+        "-noconsolecontrols", link, "&> /dev/null"
+        ])
 
 
 def get_correct_input(word):
@@ -43,43 +53,66 @@ def get_words_file(words_file="100_hfw.txt"):
     return words_file
 
 
+def generate_links_for_words(words, n):
+    """Generate and return dictionary {"word": "link"}
+    with n {key: value} pairs.
+    n words are selected randomly from the words list.
+    
+    """
+    result = {}
+    while len(result) < n:
+        word = random.choice(words)
+        link = get_audio_link(word)
+        if link:
+            result[word] = link
+    return result
+
+
+#TODO: implement functionality to download and save to TMP
+# audio files 
+#TODO: consider using threading or multiprocessing for downloading,
+# consider downloading firstly the file for the first word.
+# (Assume dictionary order is guaranteed to be insertion order 
+# from python version 3.7.
+
+
 def main():
+    fglt = Figlet()
+    print(fglt.renderText("Welcome  to  the  spelling  test!"))
+
     words_file = get_words_file()
     words = get_words(words_file)
-    ten_words = random.sample(words, 10)
-    
+    words_links = generate_links_for_words(words, 10)
+
     characters = cowsay.char_names
     # Removing ugly chracters from the list 
     for char in ['beavis', 'miki']:
         characters.remove(char)
     
-    fglt = Figlet()
-
     score = 0
-    for word in ten_words:
-        link = get_audio_link(word)
-        
+    for word in words_links:
+        link = words_links[word]
+
         i = 0
         while i < 3:
-            if link:
-                print("=" * 80)
-                play_sound(link)
-            else:
-                break
-
+            print("=" * 80)
+            play_sound(link)
             attempt = input("Spell: ")
             if attempt == word:
                 score += 1
                 congratulation = random.choice([
-                    "Great!", "Just right!", "Super!", "Very nice!", \
-                    "Excellent!", "Amazing!", "Cool!", "Well done!", \
-                    "Great job!", "Awesome!", "Wonderful!", "Fantastic!", \
-                    "Brilliant!", "Magnificent!", "Terrific!"])
+                    "Great!", "Just right!", "Super!", "Very nice!",
+                    "Excellent!", "Amazing!", "Cool!", "Well done!",
+                    "Great job!", "Awesome!", "Wonderful!", "Fantastic!",
+                    "Brilliant!", "Magnificent!", "Terrific!"
+                    ])
                 #print(fglt.renderText(congratulation)) 
                 character = random.choice(characters)
                 eval(f"cowsay.{character}('{congratulation}')")
+                play_sound(link)
                 break
-            print("Try again...")
+            elif i < 2:
+                print("Try again...")
             i += 1
         else:
             # After 3 attempts passed and no correct spelling entered:
